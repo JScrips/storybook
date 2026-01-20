f9b9f2de1f8a97aac4d8e17da7aa55bcbd42b32d
Terrance <terrancem.brown@gmail.com>
2026-01-20 09:18:25 -0600

    Updated My Buttons to look cooler

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -7,7 +7,15 @@ export function Button(props: ButtonProps) {
   return (
     <RACButton
       {...props}
-      className="bg-white dark:bg-gray-900 text-white px-4 py-2 rounded hover:bg-blue-700"
+      className="
+      cursor-pointer transition-all
+      bg-primary text-buttonText
+      px-6 py-2 rounded-lg text-shadow-lg/10 font-medium
+      inset-shadow-sm inset-shadow-white/40
+      border-defaultBorder border-b-[4px]
+      hover:brightness-110 hover:-translate-y-[1px]  hover:border-b-[6px]
+      pressed:border-b-[2px] pressed:brightness-90 pressed:translate-y-[2px]
+      "
     />
   );
 }
